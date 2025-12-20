61296c8b955d65c0f7e441a49785f652996d6e6c
ririnto <ririnto@kakao.com>
2025-12-20 23:10:16 +0900

    Update Shadow plugin to com.gradleup.shadow 9.3.0 in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.21"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.3.0"
 }
 
 group = "com.jetbrains.intellij.mcpserver"
